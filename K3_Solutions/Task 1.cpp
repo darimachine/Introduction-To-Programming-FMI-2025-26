1cd7963008db8b8c0d1509cefb66ace30ef266d8
Serhan Chavdarliev <serhi1334@gmail.com>
2025-12-14 14:54:36 +0200

    add solutions

---
+++ K3_Solutions/Task 1.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+
+size_t strLen(const char* s) {
+   if (!s) return 0;
+   size_t n = 0;
+
+   while (s[n]) n++;
+
+   return n;
+}
+
+
+bool isUpper(char c) {
+   return c >= 'A' && c <= 'Z';
+}
+bool isLower(char c) {
+   return c >= 'a' && c <= 'z';
+}
+bool isDigit(char c) {
+   return c >= '0' && c <= '9';
+}
+
+
+char toLower(char c) {
+   return c + ('a' - 'A');
+}
+
+
+char digitTransform(char c) {
+   return ('9' - c) + '0';
+}
+
+
+char otherTransform(char c) {
+   return c == '?' ? c : '*';
+}
+
+
+void transformChar(char& c) {
+   if (isUpper(c)) {
+       c = toLower(c);
+   }
+   else if (isDigit(c)) {
+       c = digitTransform(c);
+   }
+   else if (!isLower(c)) {
+       c = otherTransform(c);
+   }
+}
+
+
+
+
+void mySwap(char& a, char& b) {
+   char temp = a;
+   a = b;
+   b = temp;
+}
+
+
+void reverseRange(char* s, size_t start, size_t end) {
+   while (start < end) {
+       mySwap(s[start], s[end]);
+       start++;
+       end--;
+   }
+}
+
+
+void transformString(char* str, int k) {
+   if (!str || k <= 0) return;
+
+   size_t length = strLen(str);
+
+   for (size_t start = 0; start < length; start += k) {
+       size_t end = start + k - 1;
+       if (end >= length) {
+           end = length - 1;
+       }
+
+
+       reverseRange(str, start, end);
+   }
+
+   for (size_t i = 0; i < length; i++) {
+       transformChar(str[i]);
+   }
+}
+
+
+int main() {
+   char str[] = "AbC123?!XYZ";
+   int k = 3;
+
+   transformString(str, k);
+   cout << str << endl;
+   return 0;
+}
+
---
+++ K3_Solutions/Task 2.cpp
@@ -0,0 +1,74 @@
+#include <cstdint>
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+constexpr int TOTAL_DIGITS = 20;
+int countDigitsAndPopulateArray(long long N, int* digits) {
+    if (!digits) return -1;
+    int size = 0;
+    
+    while (N > 0) {
+        digits[size++] = N % 10;
+        N /= 10;
+    
+    }
+    return size;
+}
+
+uint8_t markRemainders(const int* digits, int size, int K) {
+
+    uint8_t seen=0;
+
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            if (i == j) { 
+                continue;
+            }
+            int currentNum = 10 * digits[i] + digits[j];
+            
+            if (currentNum < 10 || currentNum>99) {
+                continue;
+            }
+            int remainder = currentNum % K;
+            
+            if (remainder != 0) {
+                seen |= 1 << (remainder - 1);
+            }
+            
+        }
+    }
+    return seen;
+
+}
+void printRemainders(uint8_t seen, int K) {
+    for (int i = 0; i < K-1; i++)
+    {
+        if (seen & (1 << i)) {
+            cout << i + 1<<" ";
+        }
+    }
+}
+
+int main() {
+    long long N;
+    cin >> N;
+
+    int K;
+    cin >> K;
+    if (K < 2 || K>9) {
+        return -1;
+    }
+
+    int digits[TOTAL_DIGITS]{};
+    int numberLength = countDigitsAndPopulateArray(N, digits);
+    
+    uint8_t seen = markRemainders(digits, numberLength, K);
+
+    printRemainders(seen, K);
+
+}
+
