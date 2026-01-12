7a1fda117d884f4e3f8f638fd9fa269bb133a6fe
emiltrifonov <em4ito@gmail.com>
2026-01-12 22:06:30 +0200

    add labyrinth escape task solution

---
+++ Week 14/Practicum 14/labyrinth.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+void freeMatrix(bool** matrix, size_t size) {
+    for (size_t i = 0; i < size; i++) {
+        delete[] matrix[i];
+    }
+
+    delete[] matrix;
+}
+
+bool insideMatrix(size_t rows, size_t cols, int x, int y) {
+    return x >= 0 && x < rows && y >= 0 && y < cols;
+}
+
+constexpr size_t DIRECTIONS_COUNT = 4;
+int dx[DIRECTIONS_COUNT] = { 1, -1, 0, 0 };
+int dy[DIRECTIONS_COUNT] = { 0, 0, 1, -1 };
+
+bool canEscape(bool** labyrinth, bool** visited, size_t rows, size_t cols, int x, int y) {
+    if (!labyrinth || !visited) {
+        return false;
+    }
+    if (!insideMatrix(rows, cols, x, y) || visited[x][y] || !labyrinth[x][y]) {
+        return false;
+    }
+
+    visited[x][y] = true;
+
+    if (x == rows - 1 && y == cols - 1) {
+        return true;
+    }
+
+    for (size_t i = 0; i < DIRECTIONS_COUNT; i++) {
+        if (canEscape(labyrinth, visited, rows, cols, x + dx[i], y + dy[i])) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void printMatrix(bool** matrix, size_t rows, size_t cols) {
+    if (!matrix) {
+        return;
+    }
+
+    for (size_t i = 0; i < rows; i++)
+    {
+        for (size_t j = 0; j < cols; j++)
+        {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+    bool** labyrinth = new bool* [N];
+    bool** visited = new bool* [N];
+    for (size_t i = 0; i < N; i++) {
+        labyrinth[i] = new bool[M];
+        visited[i] = new bool[M] {0};
+        for (size_t j = 0; j < M; j++) {
+            cin >> labyrinth[i][j];
+        }
+    }
+
+    cout << boolalpha << canEscape(labyrinth, visited, N, M, 0, 0) << endl << endl;
+
+    //printMatrix(visited, N, M);
+
+    freeMatrix(labyrinth, N);
+    freeMatrix(visited, N);
+}
